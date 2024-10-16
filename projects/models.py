717674b4a328454847478d962cba290d3d3c841b
Guram12 <guram.shanidze.33@gmail.com>
2024-10-16 13:19:02 +0400

    new env

--- projects/models.py
+++ projects/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 
-# const electricityProjectData: Project_Data_Types[] = [
-#   { imgSrc: pdf_image, fileSize: "144 KB", downloadLink: "/test.pdf" },
-#   { imgSrc: pdf_image, fileSize: "55 KB", downloadLink: "/test.pdf" },
-#   { imgSrc: pdf_image, fileSize: "20 MB", downloadLink: "/test.pdf" },
-#   { imgSrc: pdf_image, fileSize: "46 KB", downloadLink: "/test.pdf" },
-
-
-# saxeli,
-# proeqtis surati ,
-# desqripotion 
-
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
