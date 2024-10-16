77452227cee06ea1c33cd05f21b492f36f37df7b
Guram12 <guram.shanidze.33@gmail.com>
2024-10-16 13:09:43 +0400

    Update ALLOWED_HOSTS and STATIC_ROOT in settings.py

--- mepunity/settings.py
+++ mepunity/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yq5hb8)-%u*z(m1*&+8l)=xg(h9-&7doa1i603*!$^-$r0cg*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     "projects",
     'storages',
-
 ]
 
 MIDDLEWARE = [
@@ -136,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -168,8 +168,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# settings.py
-
+# AWS S3 settings
 AWS_ACCESS_KEY_ID = 'AKIATX3PICNAR3JGAM55'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'mep-unity-bucket'
