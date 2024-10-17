4f1467909edcfd33b77400230b5cf5b432d52602
Guram12 <guram.shanidze.33@gmail.com>
2024-10-17 20:04:51 +0400

    Add image and company fields to CustomUser model

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-import logging
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import AccountSerializer
 from allauth.account.views import ConfirmEmailView
-from django.shortcuts import redirect
 from allauth.account.models import EmailConfirmationHMAC
+from django.shortcuts import redirect
+import logging
 
 
 logger = logging.getLogger(__name__)
@@ -17,9 +20,17 @@ class CustomConfirmEmailView(ConfirmEmailView):
             user = confirmation.email_address.user
             user.is_email_verified = True  
             user.save()
-            return redirect('http://localhost:5174/login')  # Redirect to frontend login page
+            return redirect('http://localhost:5173')  # Redirect to frontend login page
         else:
             logger.error("Confirmation not found, returning invalid template")
-            return redirect('http://localhost:5174/login') 
+            return redirect('http://localhost:5173/') 
         
 
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = AccountSerializer(user)
+        return Response(serializer.data)
