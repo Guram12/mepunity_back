a3dfd5175f3498db95d68b9e6d46a11573d2260c
guram12 <guram.shanidze.33@gmail.com>
2024-11-04 14:55:57 +0400

    Remove commented-out code for caching and upload progress tracking in send_file_to_email view

--- projects/views.py
+++ projects/views.py
@@ -83,15 +83,6 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
 from botocore.exceptions import NoCredentialsError
-# import threading
-# import time
-
-
-# def clear_cache_after_delay(delay):
-#     time.sleep(delay)
-#     cache.set('upload_progress', 0)
-#     logger.info("Upload progress cache cleared")
-
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -115,9 +106,8 @@ def send_file_to_email(request):
             region_name=settings.AWS_S3_REGION_NAME
         )
 
-        # total_files = len(files)
         file_urls = []
-        for i, file in enumerate(files):
+        for  file in files:
             s3_key = f"media/uploaded_data_sent_email/{company}/{file.name}"
             s3.upload_fileobj(file, settings.AWS_STORAGE_BUCKET_NAME, s3_key)
             file_url = s3.generate_presigned_url(
@@ -126,9 +116,7 @@ def send_file_to_email(request):
                 ExpiresIn=3600
             )
             file_urls.append({'file_name': file.name, 'file_url': file_url})
-            # progress = ((i + 1) / total_files) * 80  # 80% for file upload
-            # cache.set('upload_progress', progress)
-            # logger.info(f"Upload progress: {progress}%")
+
 
         context = {
             'name': name,
@@ -147,11 +135,8 @@ def send_file_to_email(request):
             recipient_list=['g.nishnianidze97@gmail.com'],
         )
 
-        # cache.set('upload_progress', 100)  # 100% after email is sent
-        # logger.info("Upload progress: 100%")
         response = Response({'message': 'Files uploaded and email sent successfully'}, status=status.HTTP_200_OK)
         
-        # threading.Thread(target=clear_cache_after_delay, args=(5,)).start()  # 5-second delay
         return response
     except NoCredentialsError:
         logger.error("AWS credentials not available.")
@@ -159,15 +144,6 @@ def send_file_to_email(request):
     except Exception as e:
         logger.error(f"Failed to send email: {str(e)}")
         return Response({'error': f'Failed to send email: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-# --------------------- get upload progress view  ---------------------
-
-# @api_view(['GET'])
-# @permission_classes([AllowAny])
-# def get_upload_progress(request):
-#     progress = cache.get('upload_progress', 0)
-#     print(progress)
-#     return Response({'progress': progress})
-
 
 # ==========================================  project price view  ==========================================
 
@@ -201,3 +177,81 @@ def project_services_list(request):
 
 
 
+
+
+
+
+
+
+
+
+
+
+# ====================es  aris musha ======================
+
+# import boto3
+# from django.conf import settings
+# from django.template.loader import render_to_string
+# from django.core.mail import send_mail
+# from botocore.exceptions import NoCredentialsError
+
+# @api_view(['POST'])
+# @permission_classes([AllowAny])
+# def send_file_to_email(request):
+#     parser_classes = (MultiPartParser, FormParser)
+#     files = request.FILES.getlist('files')
+#     name = request.data.get('name')
+#     company = request.data.get('company')
+#     email = request.data.get('email')
+#     subject = request.data.get('subject', 'No Subject')
+#     description = request.data.get('description', 'No Description')
+
+#     if not name or not company or not email:
+#         return Response({'error': 'Name, Company, and Email are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+#     try:
+#         s3 = boto3.client(
+#             's3',
+#             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+#             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+#             region_name=settings.AWS_S3_REGION_NAME
+#         )
+
+#         file_urls = []
+#         for i, file in enumerate(files):
+#             s3_key = f"media/uploaded_data_sent_email/{company}/{file.name}"
+#             s3.upload_fileobj(file, settings.AWS_STORAGE_BUCKET_NAME, s3_key)
+#             file_url = s3.generate_presigned_url(
+#                 'get_object',
+#                 Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': s3_key},
+#                 ExpiresIn=3600
+#             )
+#             file_urls.append({'file_name': file.name, 'file_url': file_url})
+
+
+#         context = {
+#             'name': name,
+#             'company': company,
+#             'email': email,
+#             'subject': subject,
+#             'description': description,
+#             'file_urls': file_urls,
+#         }
+#         message = render_to_string('accounts/email_template.html', context)
+
+#         send_mail(
+#             subject=f"File Upload: {subject}",
+#             message=message,
+#             from_email=settings.DEFAULT_FROM_EMAIL,
+#             recipient_list=['g.nishnianidze97@gmail.com'],
+#         )
+
+#         response = Response({'message': 'Files uploaded and email sent successfully'}, status=status.HTTP_200_OK)
+        
+#         return response
+#     except NoCredentialsError:
+#         logger.error("AWS credentials not available.")
+#         return Response({'error': 'AWS credentials not available'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     except Exception as e:
+#         logger.error(f"Failed to send email: {str(e)}")
+#         return Response({'error': f'Failed to send email: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
